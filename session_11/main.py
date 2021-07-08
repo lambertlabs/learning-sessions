297cb4289efdd9f22a7f37975f85619e3ea42942
George Lambert <george@lambertlabs.com>
2021-07-08 15:01:16 +0100

    chore: problem code setup

---
+++ session_11/main.py
@@ -0,0 +1,9 @@
+from problem_code import add, repeat_list_as_string
+
+print(add(1, 2))
+print(
+    repeat_list_as_string(
+        my_list=['a', 'b', 'c'],
+        number_of_repetitions=3,
+    ),
+)
---
+++ session_11/problem_code.py
@@ -0,0 +1,6 @@
+def repeat_list_as_string(my_list, number_of_repetitions):
+    return ''.join(number_of_repetitions * my_list)
+
+
+def add(a, b):
+    return a + b
